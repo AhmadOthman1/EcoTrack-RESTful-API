223f588b2af0e0b8eaaa696ff803994aa5e7ce5c
Omar Quzmar <amori@gmail.com>
2023-12-23 16:02:40 +0200

    database credentias added

--- util/database.js
+++ util/database.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
-
+/*
 const sequelize = new Sequelize('ecotrack', 'root', '123456', {
+  dialect: 'mysql',
+  host: 'localhost',
+});*/
+const sequelize = new Sequelize('ecotrack', 'root', '', {
   dialect: 'mysql',
   host: 'localhost',
 });
