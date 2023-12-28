3f736e72600014b2b96424bf9dd769518a7ce9cb
Omar Quzmar <amori@gmail.com>
2023-12-28 23:10:12 +0200

    last edit

--- controller/dataCollectionController.js
+++ controller/dataCollectionController.js
@@ -3,9 +3,10 @@ const { Op } = require("sequelize");
 const DataCollection = require("../models/dataCollection");
 const Location = require("../models/location");
 const User = require("../models/user");
-const Alert = require("../models/alert");
+const Alert = require("../models/Alert");
 const Interests = require("../models/interests");
 const UserInterests = require("../models/userInterests");
+const Data = require("../models/data");
 const nodemailer = require("nodemailer");
 
 const ensureDataCollectionModifier = (dataCollection, userId) => {
@@ -28,21 +29,26 @@ const getDataCollectionById = async (req, res) => {
     if (!dataCollection) {
       return res.status(404).json({ error: "Data collection not found" });
     }
-    res.status(200).json(dataCollection);
+    const populatedDataCollection = await DataCollection.findByPk(id, {
+      include: [{ model: Data }],
+    });
+
+    res.status(201).json(populatedDataCollection);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
 const createDataCollection = async (req, res) => {
-  const { location, interests, description, date } = req.body;
+  const { location, interests, dataArray, description, date } = req.body;
+  const interstsArray = interests;
   const user = req.user;
 
   userId = user.userId;
   try {
-    if (interests.length < 1 || interests.length > 255) {
+    if (interests.length < 1 || interests.length > 511) {
       return res.status(409).json({
-        message: interest + "is Not a Valid interests (length must be: 1-255)",
+        message: interest + "is Not a Valid interests (length must be: 1-511)",
         body: req.body,
       });
     }
@@ -68,18 +74,57 @@ const createDataCollection = async (req, res) => {
         body: req.body,
       });
     }
+    if (
+      !Array.isArray(dataArray) &&
+      dataArray.length < 1 &&
+      dataArray.length > 63
+    ) {
+      return res.status(409).json({
+        message: "interests should be array, and contains 1-63 data ",
+        body: req.body,
+      });
+    }
+
+    for (let i = 0; i < dataArray.length; i++) {
+      if (!ValidateDataSingle(dataArray[i]))
+        return res.status(409).json({
+          message: `your data at position ${i + 1} is not valid`,
+          body: req.body,
+        });
+    }
+
     const newDataCollection = await DataCollection.create({
       userId,
       location,
-      interests,
+      interests: interstsArray.join(","),
       description,
       date,
     });
+
+    let dataStorePromises = [];
+    for (let dataSingle of dataArray) {
+      let prom = Data.create({
+        dateType: dataSingle.dateType,
+        DataValue: dataSingle.DataValue,
+        dataCollectionId: newDataCollection.dataCollectionId,
+      });
+
+      dataStorePromises.push(prom);
+    }
+
+    await Promise.all(dataStorePromises);
     const userFromDb = await User.findByPk(userId);
     await userFromDb.increment("score", { by: 1 });
 
-    notifyIntersted(newDataCollection);
-    res.status(201).json(newDataCollection);
+    notifyIntersted(newDataCollection, interstsArray);
+    const populatedDataCollection = await DataCollection.findByPk(
+      newDataCollection.dataCollectionId,
+      {
+        include: [{ model: Data }],
+      }
+    );
+
+    res.status(201).json(populatedDataCollection);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
     console.log(error);
@@ -87,7 +132,7 @@ const createDataCollection = async (req, res) => {
 };
 
 const updateDataCollection = async (req, res) => {
-  const { location, interests, description, date } = req.body;
+  const { location, interests, dataArray, description, date } = req.body;
 
   const { id } = req.params;
   try {
@@ -103,6 +148,7 @@ const updateDataCollection = async (req, res) => {
         .status(403)
         .json({ error: "You aren't the creator of the collection" });
     }
+    req.body.interests = req.body.interests.join(",");
 
     if (interests.length < 1 || interests.length > 255) {
       return res.status(409).json({
@@ -133,8 +179,58 @@ const updateDataCollection = async (req, res) => {
       });
     }
 
+    if (
+      !Array.isArray(dataArray) &&
+      dataArray.length < 1 &&
+      dataArray.length > 63
+    ) {
+      return res.status(409).json({
+        message: "interests should be array, and contains 1-63 data ",
+        body: req.body,
+      });
+    }
+
+    for (let i = 0; i < dataArray.length; i++) {
+      if (!ValidateDataSingle(dataArray[i]))
+        return res.status(409).json({
+          message: `your data at position ${i + 1} is not valid`,
+          body: req.body,
+        });
+    }
+
+    const dataToDelete = await Data.findAll({
+      where: {
+        dataCollectionId: id,
+      },
+    });
+
+    if (dataToDelete.length > 0) {
+      const deletePromises = dataToDelete.map((data) => {
+        return data.destroy();
+      });
+
+      await Promise.all(deletePromises);
+    }
+
     await dataCollection.update(req.body);
-    res.status(200).json(dataCollection);
+
+    let dataStorePromises = [];
+    for (let dataSingle of dataArray) {
+      let prom = Data.create({
+        dateType: dataSingle.dateType,
+        DataValue: dataSingle.DataValue,
+        dataCollectionId: id,
+      });
+
+      dataStorePromises.push(prom);
+    }
+
+    await Promise.all(dataStorePromises);
+    const populatedDataCollection = await DataCollection.findByPk(id, {
+      include: [{ model: Data }],
+    });
+
+    res.status(201).json(populatedDataCollection);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal server error" });
@@ -159,24 +255,30 @@ const deleteDataCollection = async (req, res) => {
 
     res.status(204).end();
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-const notifyIntersted = async (dataCollection) => {
-  const interest = dataCollection.interests;
-  const users = await findUsersByInterest(interest);
-  users.forEach(async (user) => {
-    notifySingleIntersted(user, dataCollection);
-    console.log("user notified");
-  });
+const notifyIntersted = async (dataCollection, interestsArray) => {
+  for (interest of interestsArray) {
+    const users = await findUsersByInterest(interest);
+    users.forEach(async (user) => {
+      notifySingleIntersted(user, dataCollection);
+      console.log("user notified");
+    });
+  }
 };
 const notifySingleIntersted = async (user, dataCollection) => {
   const alerter = Alert.create({
     dataCollectionId: dataCollection.dataCollectionId,
     userId: user.userId,
   });
-  const mailer = sendEmailForInterestNotify(user.email, dataCollection);
+  const mailer = sendEmailForInterestNotify(
+    user.email,
+    dataCollection,
+    interest
+  );
   Promise.all([alerter, mailer]);
 };
 
@@ -218,7 +320,11 @@ async function findUsersByInterest(interestKeyword) {
   }
 }
 
-const sendEmailForInterestNotify = async (destEmail, dataCollection) => {
+const sendEmailForInterestNotify = async (
+  destEmail,
+  dataCollection,
+  interest
+) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -231,13 +337,23 @@ const sendEmailForInterestNotify = async (destEmail, dataCollection) => {
     from: "advanced.2001z@gmail.com",
     to: destEmail,
     subject: "EcoTrack Updates!!",
-    text: `There's updates related to your interst: ${dataCollection.interests} and its descriped by ${dataCollection.description}`,
+    text: `There's updates related to your interst: ${interest} and its descriped by ${dataCollection.description}`,
   };
 
   // Send the email
   try {
     const info = await transporter.sendMail(mailOptions);
   } catch {}
+};
+
+const ValidateDataSingle = (dataSingle) => {
+  console.log(dataSingle);
+  if (dataSingle.dateType.length < 2 && dataSingle.dateType.length > 255)
+    return false;
+  if (dataSingle.DataValue.length < 1 && dataSingle.DataValue.length > 255)
+    return false;
+
+  return true;
 };
 module.exports = {
   getAllDataCollections,
